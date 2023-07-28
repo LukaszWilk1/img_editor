4c1917619580dbe2b3d8401b933ebec9d95e5cd5
Łukasz Wilk <107866108+LukaszWilk1@users.noreply.github.com>
2023-07-28 17:29:25 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -23,3 +23,38 @@ class Picture {
 function updateState(state, action){                                            // This function is returns new object that will have new state and new actions it it
   return Object.assign({}, state, action);                                      // {} represents new object and state and action are values that will be added to it
 }
+
+ function elt(type, props, ...children){                                        // This function is responsible for creating dom elements. ... means that we are including all instances of object from it to another object
+  let dom = document.createElement(type);                                       // dom variable holds actual DOM element which have type of type
+  if(props) Object.assign(dom,props);
+  for(let child of children){
+    if(typeof child != "string") dom.appendChild(child);                        // If child is not a string it adds element to end of children list on page 
+    else dom.appendChild(document.createTextNode(child));                            // If child is a string it creates text on page
+  }
+  return dom;
+ }
+
+const scale = 10;
+class PictureCanvas {
+  constructor(picture, pointerDown) {
+    this.dom = elt("canvas",{onmousedown: event => this.mouse(event, pointerDown), ontouchstart: event => this.mouse(event, pointerDown)});
+    this.syncState(picture);
+  }
+  syncState(picture) {
+    if(this.picture==picture) return;
+    this.picture = picture;
+    drawPicture(this.picture, this.dom, scale);
+  }
+}
+
+function drawPicture(picture, canvas, scale){
+  canvas.width = picture.width*scale;
+  canvas.height = picture.height*scale;
+  let cx = canvas.getContext("2d");
+  for(let y=0; y<picture.height;y++){
+    for(let x=0; x<picture.wdith;x++){
+      cx.fillStyle = picture.pixel(x,y);
+      cx.fillRect(x*scale,y*scale,scale,scale);
+    }
+  }
+}
