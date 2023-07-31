17b68867e7214dca0c37d701f62a0841bda8e66e
Łukasz Wilk <107866108+LukaszWilk1@users.noreply.github.com>
2023-07-31 10:19:01 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function updateState(state, action){                                            
   let dom = document.createElement(type);                                       // dom variable holds actual DOM element which have type of type
   if(props) Object.assign(dom,props);
   for(let child of children){
-    if(typeof child != "string") dom.appendChild(child);                        // If child is not a string it adds element to end of children list on page 
+    if(typeof child != "string") dom.appendChild(child);                        // If child is not a string it adds element to end of children list on page
     else dom.appendChild(document.createTextNode(child));                       // If child is a string it creates text on page
   }
   return dom;
@@ -39,7 +39,7 @@ class PictureCanvas {
   constructor(picture, pointerDown) {
     this.dom = elt("canvas",{onmousedown: event => this.mouse(event, pointerDown), ontouchstart: event => this.touch(event, pointerDown)});     // this.dom element holds our canvas with 2 events and functions assigned to them
                                                                                                                                                 // Argument event holds object of MouseEvent or TouchEvent which has its own instances. We pass this objects to mouse and touch methods
-    this.syncState(picture);                                                                                                                    // Every time we create PictureCanvas we sync state which is assigning new picture to old one if they are diferent and then we draw this 
+    this.syncState(picture);                                                                                                                    // Every time we create PictureCanvas we sync state which is assigning new picture to old one if they are diferent and then we draw this
                                                                                                                                                 // picture using drawPicture funcion
   }
   syncState(picture) {
@@ -61,7 +61,7 @@ function drawPicture(picture, canvas, scale){                                   
   }
 }
 
-PictureCanvas.prototype.mouse = function  (downEvent, onDown){
+PictureCanvas.prototype.mouse = function(downEvent, onDown){
   if(downEvent.button != 0) return;
   let pos = pointerPosition(downEvent, this.dom);
   let onMove = onDown(pos);
@@ -102,4 +102,92 @@ PictureCanvas.prototype.touch = function(startEvent, onDown){
   };
   this.dom.addEventListener("touchmove", move);
   this.dom.addEventListener("touchend", end);
+}
+
+class PixelEditor {
+  constructor(state, config){
+    let {tools, controls, dispatch} = config;
+    this.state=state;
+    this.canvas = new PictureCanvas(state.picture, pos => {
+      let tool = tools[this.state.tool];
+      let onMove = tool(pos, this.state, dispatch);
+      if(onMove) return pos => onMove(pos, this.state);
+    });
+    this.controls = controls.map(
+      Control => new Control(state, config));
+      this.dom = elt("div", {}, this.canvas.dom, elt("br"),
+    ...this.controls.reduce(
+      (a, c) => a.concat(" ", c.dom), []));
+  }
+  syncState(state) {
+    this.state = state;
+    this.canvas.syncState(state.picture);
+    for(let ctrl of this.controls) ctrl.syncState(state);
+  }
+}
+
+class ToolSelect {
+  constructor(state, {tools, dispatch}) {
+    this.select = elt("select", {
+      onchange: () => dispatch({tool: this.select.value})
+    }, ...Object.keys(tools).map(name => elt("option", {
+      selected: name == state.tool
+    }, name)));
+    this.dom = elt("label", null, "Narzędzie: ", this.select);
+  }
+  syncState(state) {this.select.value=state.tool;}
+}
+
+class ColorSelect {
+  constructor(state, {dispatch}) {
+    this.input = elt("input", {
+      type: "color", value: state.color,
+      onchange: () => dispatch({color: this.input.value})
+    });
+    this.dom = elt("label", null, "Kolor: ", this.input);
+  }
+  syncState(state) {this.input.value=state.color;}
+}
+
+function draw(pos, state, dispatch){
+  function drawPixel({x ,y}, state){
+    let drawn = {x, y, color: state.color};
+    dipatch({picture: state.picture.draw([drawn])});
+  }
+  drawPixel(pos, state);
+  return drawPixel;
+}
+
+function rectangle(start, state, dispatch){
+  function drawRectangle(pos){
+    let xStart = Math.min(start.x, pos.x);
+    let yStart = Math.min(start.y, pos.y);
+    let xEnd = Math.max(start.x, pos.x);
+    let yEnd = Math.max(start.y, pos.y);
+    let drawn = [];
+    for(let y = yStart; y <= yEnd; y++){
+      for(let x= xStart; x< xEnd; x++){
+        drawn.push({x, y, color: state.color});
+      }
+    }
+    dispatch({picture: state.picture.draw(drawn)});
+  }
+  drawRectangle(start);
+  return drawRectangle;
+}
+
+const around = [{dx: -1, dy: 0}, {dx: 1, dy: 0},
+{dx: 0, dy: -1}, {dx: 0, dy: 1}];
+function fill({x, y}, state, dispatch){
+  let targetColor = state.picture.pixel(x, y);
+  let drawn = [{x, y, color: state.color}];
+  for(let done = 0; done < drawn.length; done++){
+    for(let {dx, dy} of around){
+      let x = drawn[done].x +dx, y = drawn[done].y+dy;
+      if(x>=0 && x < state.picture,width && y >= 0 && y < state.picture.height && state.picture.pixel(x, y) == targetColor && !drawn.some(p => p.x==x && p.y==y)) {
+        drawn.push({x, y, color: state.color});
+      }
+    }
+  }
+  dispatch({picture: state.picture.draw(drawn)});
 }
