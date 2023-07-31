c93ee6925fac2cdfc2a7736eeda4f18e986a6c1d
Łukasz Wilk <107866108+LukaszWilk1@users.noreply.github.com>
2023-07-31 10:53:52 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -191,3 +191,79 @@ function fill({x, y}, state, dispatch){
   }
   dispatch({picture: state.picture.draw(drawn)});
 }
+
+function pick(pos, state, dispatch){
+  dispatch({color: state.picture.pixel(pos.x, pos.y)});
+}
+
+class SaveButton {
+  constructor(state){
+    this.picture = state.picture;
+    this.dom = elt("button", {
+      onclick: () => this.save()
+    }, "Zapisz");
+  }
+  save(){
+    let canvas = elt("canvas");
+    drawPicture(this.picture, canvas, 1);
+    let link = elt("a", {
+      href: canvas.toDataURL(),
+      download: "pixelart.png"
+    });
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+  }
+  syncState(state) {this.picture = state.picture;}
+}
+
+class LoadButton {
+  constructor(_, {dispatch}) {
+    this.dom = elt("button", {
+      onclick: () => startLoad(dispatch)
+    }, "Wczytaj");
+  }
+  syncState() {}
+}
+
+function startload(){
+  let input = elt("input", {
+    type: "file",
+    onchange: () => finishLoad(input.files[0], dispatch)
+  });
+  document.body.appendChild(input);
+  input.click();
+  input.remove();
+}
+
+function finishLoad(file, dispatch) {
+  if(file == null) return;
+  let reader = new FileReader();
+  reader.addEventListener("load", () => {
+    let image = elt("img", {
+      onload: () => dispatch({
+        picture: pictureFromImage(image)
+      }),
+      src: reader.result
+    });
+  });
+  reader.readAsDataURL(file);
+}
+
+function pictureFromImage(image){
+  let width = Math.min(100, image.width);
+  let height = Math.min(100, image.height);
+  let canvas = elt("canvas", {width, height});
+  let cx = canvas.getContext("2d");
+  cx.drawImage(image, 0, 0);
+  let pixels = [];
+  let {data} = cx.getImageData(0, 0, width, height);
+  function hex(n) {
+    return n.toString(16).padStart(2, "0");
+  }
+  for(let i = 0; i<data.length; i+= 4){
+    let [r, g, b] = data.slice(i, i+3);
+    pixels.push("#" + hex(r) + hex(g) + hex(b));
+  }
+  return new Picture(width, height, pixels);
+}
